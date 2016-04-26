c5908bc3802500d10b90ae439064705eab99c8be
lqdung <lqdung>
2016-04-26 09:46:06 +0700

    Category Content

---
+++ app/Plugin/CategoryContent/Migration/Version20160425092000.php
@@ -0,0 +1,28 @@
+<?php
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20160425092000 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+    	$this->createPluginTable($schema);
+    }
+
+    public function down(Schema $schema)
+    {
+    	$schema->dropTable('plg_category_content');
+    }
+
+    protected function createPluginTable(Schema $schema)
+    {
+        $table = $schema->createTable("plg_category_content");
+        $table->addColumn('category_id', 'integer');
+        $table->addColumn('content', 'text', array('notnull' => false));
+        $table->addColumn('image', 'text', array('notnull' => false));
+        $table->setPrimaryKey(array('category_id'));
+
+    }
+}
