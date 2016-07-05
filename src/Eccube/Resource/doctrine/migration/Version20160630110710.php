86bc72ad220ee7f73a0c965cdb79906b2ff0ff4c
lqdung <lqdung@lockon-vn.com>
2016-07-05 17:21:04 +0700

    [REFACTOR] Refactor code + fix code style.

--- src/Eccube/Resource/doctrine/migration/Version20160630110710.php
+++ src/Eccube/Resource/doctrine/migration/Version20160630110710.php
@@ -4,6 +4,7 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Eccube\Entity\Master\DeviceType;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -17,14 +18,14 @@ class Version20160630110710 extends AbstractMigration
     {
         $app = \Eccube\Application::getInstance();
 
-        $DeviceType = $app['eccube.repository.master.device_type']->find(10);
+        $DeviceType = $app['eccube.repository.master.device_type']->find(DeviceType::DEVICE_TYPE_PC);
 
         $PageLayout = $app['eccube.repository.page_layout']->findOneBy(array(
             'url' => 'mypage_delivery_edit',
         ));
         if (!$PageLayout) {
             $PageLayout = new \Eccube\Entity\PageLayout();
-            $PageLayout->setName('マイページ/お届け先編集')
+            $PageLayout->setName('MYページ/お届け先編集')
                 ->setDeviceType($DeviceType)
                 ->setUrl('mypage_delivery_edit')
                 ->setFileName('Mypage/delivery_edit')
